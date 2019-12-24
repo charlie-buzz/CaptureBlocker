c2960aad9f7c0ee1ee0f29a67c7ab64e37709145
ChanHee Kim <chanhee.k@webtoonscorp.com>
2019-12-24 19:26:34 +0900

    .gitignore is now working

---
+++ Example/build/Pods.build/Debug-iphoneos/CaptureBlocker.build/DerivedSources/CaptureBlocker_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char CaptureBlockerVersionString[];
+ extern const double CaptureBlockerVersionNumber;
+
+ const unsigned char CaptureBlockerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CaptureBlocker  PROJECT:Pods-1" "\n";
+ const double CaptureBlockerVersionNumber __attribute__ ((used)) = (double)1.;
