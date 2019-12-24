11e7f93476a3774b907ad6120f8001e082eda537
ChanHee Kim <chanhee.k@webtoonscorp.com>
2019-12-24 19:52:37 +0900

    WIP

---
+++ Source/CBImageView.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class CBImageView: UIImageView, CaptureBlockable {
+    
+}
+
+class CBView: UIView, CaptureBlockable {
+    
+}
+
+class CBLabel: UILabel, CaptureBlockable {
+    
+}
+
+class CBTextView: UITextView, CaptureBlockable {
+    
+}
--- Source/CaptureBlockable.swift
+++ Source/CaptureBlockable.swift
@@ -1,5 +1,25 @@
 import Foundation
 
-protocol CaptureBlockable {
+protocol CaptureBlockable where Self: UIView {
+
+    var replacementView: UIView? { get set }
+
+}
+
+extension CaptureBlockable {
+
+    func setEnabled(_ isEnabled: Bool) {
+        CBManager.shared.setEnabled(self, isEnabled: isEnabled)
+    }
+
+}
+
+private class CBManager {
+
+    static var shared = CBManager()
     
+    func setEnabled(_ obj: CaptureBlockable, isEnabled: Bool) {
+        
+    }
+
 }
